c660c77c6b7cced39bf71bd74af5a94a76ce4113
sparklweb1988 <sparklewebacademy@gmail.com>
2026-03-10 10:12:17 +0100

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-02y&z_wh2iw0b_tua_xr87akk+%e5_hdn1=$=d*aywrl@u%9^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
